6f8ece4d10878d01dca98e0958decd35b3fc796e
Joanna Ingram <s3387644@student.rmit.edu.au>
2019-10-10 13:23:57 +1100

    Initial Commit

---
+++ Medijuana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medijuana
+//
+//  Created by Joanna Ingram on 10/10/19.
+//  Copyright © 2019 Joanna Ingram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
