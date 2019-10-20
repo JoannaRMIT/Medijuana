22e539f1f9e880f1d405f2a53ee49cb5d22aebeb
Joanna Ingram <s3387644@student.rmit.edu.au>
2019-10-20 11:36:18 +1100

    Over 18 alert

--- Medijuana/Controller/ViewController.swift
+++ Medijuana/Controller/ViewController.swift
@@ -12,7 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let alertController = UIAlertController(title: "Medijuana", message: "Content on this app is only suitable for those over 18. Are you 18 or over?", preferredStyle: .alert)
+        
+        let yesButton = UIAlertAction(title: "Yes", style: .default, handler: {(action)->Void in print("User is over 18")})
+        
+        let noButton = UIAlertAction(title: "No", style: .default, handler: {(action)->Void in print("User is under 18")})
+        
+        alertController.addAction(yesButton)
+        alertController.addAction(noButton)
+        
+        self.navigationController!.present(alertController, animated: true, completion: nil)
     }
 
 
--- Medijuana/Model/Strains.swift
+++ Medijuana/Model/Strains.swift
@@ -1,9 +1,72 @@
 //
-//  Strains.swift
-//  Medijuana
+//  Model.swift
+//  JSON Parsing
 //
-//  Created by Joanna Ingram on 10/10/19.
-//  Copyright © 2019 Joanna Ingram. All rights reserved.
+//  Created by Jessica Darvell on 19/10/19.
+//  Copyright © 2019 Jessica Darvell. All rights reserved.
 //
 
 import Foundation
+
+struct Strains {
+    
+    //Creates empty tuple for our strains
+    var strainsTuple:[(name: String, id: String, flavours:String, postive: String, negative: String, medical: String, favourite: Bool)]
+    
+    private var count = 0
+    
+//    init() {
+//        populateStrains()
+//    }
+    
+    //Populate the database (tuple)
+    mutating func populateStrains() {
+        var newStrain = ("Afpak", "1", "Earthy", "Relaxed", "Dizzy", "Depression", false)
+        strainsTuple.append(newStrain)
+        newStrain = ("Afpak", "2", "Earthy", "Relaxed", "Dizzy", "Depression", false)
+        strainsTuple.append(newStrain)
+    }
+    
+    // Function used to return individual strains via name
+//    func getStrain(name:String) -> (name: String, id: String, flavours:String, postive: String, negative: String, medical: String, favourite: Bool) {
+//        
+//    }
+    
+    // Function to get the next strain
+    mutating func getNextStrain() -> (name: String, id: String, flavours:String, postive: String, negative: String, medical: String, favourite: Bool) {
+        
+        let strain = strainsTuple[count]
+        if(count == strainsTuple.count - 1){
+            count = 0
+        }
+        else {
+            count += 1
+        }
+        return strain
+    }
+    
+    //Function is used to return Favourite list for favourite view.
+//    func getFavourites() ->  {
+//        var _:[(name: String, id: String, flavours:String, postive: String, negative: String, medical: String, favourite: Bool)]
+//
+//
+//    }
+    
+    //Function called when the 'heart' is selected to add to favourites.
+//    mutating func addToFavourites(Obj: Strain) {
+//        let Strain = strain
+//
+//
+//
+//    }
+    
+    
+    //Function called for search
+    func searchResult() -> () {
+        
+        
+    }
+    
+    
+}
+
